ddf62c9025b41fcedb96a324d3ceda62dd8cbf93
gsi-yoan <yoan@generalsoftwareinc.com>
2017-03-22 18:33:45 -0400

    Add composer

--- src/Endpoints/Facebook.php
+++ src/Endpoints/Facebook.php
@@ -11,5 +11,19 @@ namespace Marketing\Endpoints;
 
 trait Facebook
 {
+    protected $response;
 
+    public abstract function getUser();
+
+    public abstract function getFacebook();
+
+    public function campaigns()
+    {
+        return json_encode(['data' => [['name' => 'Testing Campaign Name'], ['name' => 'Testing Campaign Name']]]);
+    }
+
+    public function accounts()
+    {
+        return json_encode(['data' => $this->getUser()]);
+    }
 }
